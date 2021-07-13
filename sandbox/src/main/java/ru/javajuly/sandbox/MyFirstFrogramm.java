fb80aec8a25d95c31f4a53609da4b7c26039f5cc
nyohisi <79828785+nyohisi@users.noreply.github.com>
2021-07-13 22:50:35 +0300

    Создан пакет для моей первой программы

--- sandbox/src/main/java/ru/javajuly/sandbox/MyFirstFrogramm.java
+++ sandbox/src/main/java/ru/javajuly/sandbox/MyFirstFrogramm.java
@@ -1,5 +1,7 @@
+package ru.javajuly.sandbox;
+
 public class MyFirstFrogramm {
   public static void main (String[] args) {
-    System.out.println("Hello, World!"); 
+    System.out.println("Hello, World ~");
   }
 }
